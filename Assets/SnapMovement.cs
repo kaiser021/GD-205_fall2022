7ba1e6b67290789bcd96853ad9779f31277976b1
kaiser021 <55509250+kaiser021@users.noreply.github.com>
2022-09-29 18:42:47 -0400

    initial unity code

---
+++ Assets/SnapMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+       if(Vector3.Distance(transform.position, movePoint.position) == 0f) { 
+
+        if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) 
+        {
+            movePoint.position -= new Vector3(Input.GetAxisRaw("Vertical"), 0f, 0f);
+        }
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+        {
+            movePoint.position += new Vector3(0f, 0f, Input.GetAxisRaw("Horizontal"));
+        }
+           
+        }
+    }
+}
